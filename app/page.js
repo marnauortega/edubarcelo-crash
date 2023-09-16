1c1431a225c2f6afc0ef9643461aeef0f843d001
Martí Arnau <marnauortega@gmail.com>
2023-09-16 19:51:26 +0200

    test(remove animation)

--- app/page.js
+++ app/page.js
@@ -1,16 +1,7 @@
-import { getHomeAnimationsCategory } from "@/lib/swell/queries/homeAnimationscategory";
-import AnimatedSequence from "@/components/AnimatedSequence/AnimatedSequence";
-
 import styles from "./page.module.css";
 
 const Home = async () => {
-  const homeImages = await getHomeAnimationsCategory();
-
-  return (
-    <div className={styles.home}>
-      <AnimatedSequence projectImages={homeImages} />
-    </div>
-  );
+  return <div className={styles.home}>Nothing</div>;
 };
 
 export default Home;
